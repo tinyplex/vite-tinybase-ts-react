e80ec8c459930e05421d9a2c312d66d44cb3cec1
James Pearce <github@tripleodeon.com>
2025-08-28 08:43:02 -1000

    [hygiene] Dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import { StrictMode } from 'react';
-import { createStore } from 'tinybase';
-import { Provider, useCreateStore } from 'tinybase/ui-react';
-import {
-  SortedTableInHtmlTable,
-  ValuesInHtmlTable,
-} from 'tinybase/ui-react-dom';
-import { Inspector } from 'tinybase/ui-react-inspector';
-import { Buttons } from './Buttons';
+import {StrictMode} from 'react';
+import {createStore} from 'tinybase';
+import {Provider, useCreateStore} from 'tinybase/ui-react';
+import {SortedTableInHtmlTable, ValuesInHtmlTable} from 'tinybase/ui-react-dom';
+import {Inspector} from 'tinybase/ui-react-inspector';
+import {Buttons} from './Buttons';
 
 export const App = () => {
   const store = useCreateStore(() => {
     // Create the TinyBase Store and initialize the Store's data
     return createStore()
       .setValue('counter', 0)
-      .setRow('pets', '0', { name: 'fido', species: 'dog' })
+      .setRow('pets', '0', {name: 'fido', species: 'dog'})
       .setTable('species', {
-        dog: { price: 5 },
-        cat: { price: 4 },
-        fish: { price: 2 },
-        worm: { price: 1 },
-        parrot: { price: 3 },
+        dog: {price: 5},
+        cat: {price: 4},
+        fish: {price: 2},
+        worm: {price: 1},
+        parrot: {price: 3},
       });
   });
 
@@ -28,10 +25,17 @@ export const App = () => {
       <Provider store={store}>
         <header>
           <h1>
-            <img src='/favicon.svg' />
+            <img src="/favicon.svg" />
             TinyBase & React
           </h1>
         </header>
+        <p>
+          This is more or less the simplest TinyBase app you could imagine. It
+          contains a handful of key-values and tables which are mutated with the
+          buttons below, and rendered with plain table components. See the full
+          set of <a href="https://tinybase.org/demos/">TinyBase demos</a> for
+          more interesting examples!
+        </p>
         <Buttons />
         <div>
           <h2>Values</h2>
@@ -40,11 +44,11 @@ export const App = () => {
         <div>
           <h2>Pets Table</h2>
           <SortedTableInHtmlTable
-            tableId='pets'
-            cellId='name'
+            tableId="pets"
+            cellId="name"
             limit={5}
             sortOnClick={true}
-            className='sortedTable'
+            className="sortedTable"
             paginator={true}
           />
         </div>
